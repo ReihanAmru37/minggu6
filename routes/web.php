376f3ca1ae7dfa1a9394114d95c7a1987d4bc50e
ReihanAmru37 <amru3.RA@gmail.com>
2021-10-18 03:52:57 +0700

    minggu6

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,5 +20,3 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::resource('students', StudentController::class);
