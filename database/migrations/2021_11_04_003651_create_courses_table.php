09fdee60eca738670ba7cd073c066de57e473046
ReihanAmru37 <amru3.RA@gmail.com>
2021-11-04 07:51:16 +0700

    minggu9

--- database/migrations/2021_11_04_003651_create_courses_table.php
+++ database/migrations/2021_11_04_003651_create_courses_table.php
@@ -15,6 +15,9 @@ class CreateCoursesTable extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
+            $table->string('course_name',30);
+            $table->integer('sks');
+            $table->integer('semester');
             $table->timestamps();
         });
     }
