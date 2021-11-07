bfc7598e620ece7ce950c33189459eb118a3f979
ReihanAmru37 <amru3.RA@gmail.com>
2021-11-08 02:05:11 +0700

    Tambah Fitur Cetak PDF

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,5 @@ Route::resource('students', StudentController::class);
 Route::resource('users', UserController::class);
 
 Route::get('/search', [StudentController::class, 'search'])->name('search');
+
+Route::get('/students/{id}/report', [StudentController::class, 'report']);
