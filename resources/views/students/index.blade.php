69e26fdf52f2d0e587fa272cc8547bc1db8aa279
ReihanAmru37 <amru3.RA@gmail.com>
2021-11-08 01:53:56 +0700

    minggu6

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -3,33 +3,33 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-    <div class="col-md-8">
-        <div class="card">
-            <div class="card-header">{{ __('STUDENT DATA') }}</div>
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('STUDENT DATA') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
                     @endif
-                    
+
                     <form class="form" method="get" action="{{ route('search') }}">
                         <div class="form-group w-100 mb-3">
                             <label for="search" class="d-block mr-2">Pencarian</label>
-                            <input type="text" name="search" class="form-control w-75 d-inline" id="search" placeholder="Masukkan nama">
+                            <input type="text" name="search" class="form-control w-75 d-inline" id="search" placeholder="Masukkan keyword">
                             <button type="submit" class="btn btn-primary mb-1">Cari</button>
                         </div>
                     </form>
-                    <!-- Start kode untuk form pencarian -->
+
                     @if ($message = Session::get('success'))
-                    <div class="alert alert-success">
-                        <p>{{ $message }}</p>
+                    <div class="alert-success">
+                        <p> {{ message }} </p>
                     </div>
                     @endif
+                    
+                    <a href="/students/create" class="btn btn-primary">Add Data</a> <br><br>
 
-                    <a href="/students/create" class="btn btn-primary">Add Data</a>
-                    <br><br>
                     <table class="table table-responsive table-striped">
                         <thead>
                             <tr>
@@ -37,7 +37,7 @@
                                 <th>Name</th>
                                 <th>Class</th>
                                 <th>Department</th>
-                                <th>Action</th> 
+                                <th>Action</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -46,15 +46,18 @@
                                 <td>{{ $s->nim }}</td>
                                 <td>{{ $s->name }}</td>
                                 <td>{{ $s->kelas->class_name }}</td>
+                                <td>{{ $s->department }}</td>
                                 <td>
+
                                     <form action="/students/{{$s->id}}" method="post">
+                                        <a href="/students/{{$s->id}}" class="btn btn-success">View</a>
                                         <a href="/students/{{$s->id}}/edit" class="btn btn-warning">Edit</a>
-                                        <a href="/students/{{$s->id}}" class="btn btn-info">View</a>
                                         @csrf
                                         @method('DELETE')
                                         <button type="submit" name="delete" class="btn btn-danger">Delete</button>
+                                        <a href="/students/{{$s->id}}/detail" class="btn btn-success">Nilai</a>
                                     </form>
-                                </td>
+                                </td>    
                             </tr>
                             @endforeach
                         </tbody>
@@ -64,3 +67,4 @@
         </div>
     </div>
 </div>
+@endsection
